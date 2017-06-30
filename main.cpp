76bc8b5c48728541706095407936fc832eee141b
qianqian121 <qianqian141@hotmail.com>
2017-06-30 14:55:20 -0700

    video capture tutorial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include<iostream>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, char *argv[]) {
     std::cout << "hello" << std::endl;
+    std::string fileName = "video.avi";
+    VideoCapture cap;
+    cap.open(fileName);
+
+    while(1)
+    {
+        Mat frame;
+
+        bool bSuccess = cap.read(frame); // read a new frame from video
+
+         if (!bSuccess) //if not success, break loop
+        {
+                        cout << "Cannot read the frame from video file" << endl;
+                       break;
+        }
+
+        imshow("MyVideo", frame); //show the frame in "MyVideo" window
+
+        if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+       {
+                cout << "esc key is pressed by user" << endl;
+                break;
+       }
+    }
+
     return 0;
 }
